11a025848f02e44e1ac3d2a9ae376c17a2feaf22
Vadim PLoskarev <vvploskarev@mail.ru>
2024-09-18 11:20:41 +0300

    fix formatation

--- ff.h
+++ ff.h
@@ -2,26 +2,23 @@
 
 #include <linux/types.h>
 
-typedef struct ff
-{   
-    uint8_t char_p;
-    uint8_t deg; 
-    uint8_t *coeff; 
+typedef struct ff {
+  uint8_t char_p;
+  uint8_t deg;
+  uint8_t *coeff;
 } ff_t;
 
-typedef struct ff_elem
-{
-    ff_t *ff;
-    uint8_t deg; 
-    uint8_t *coeff;
+typedef struct ff_elem {
+  ff_t *ff;
+  uint8_t deg;
+  uint8_t *coeff;
 } ff_elem_t;
-
 
 extern ff_t ff_d8_p2;
 extern ff_t ff_d16_p2;
 extern ff_t ff_d32_p2;
 
-void ff_elem_free (ff_elem_t *m);
+void ff_elem_free(ff_elem_t *m);
 int ff_elem_cmp(ff_elem_t *a, ff_elem_t *b);
 ff_elem_t *ff_get_zero(ff_t *ff);
 ff_elem_t *ff_get_one(ff_t *ff);
--- module.c
+++ module.c
@@ -1,3 +1,4 @@
+#include <asm/uaccess.h>
 #include <linux/atomic.h>
 #include <linux/cdev.h>
 #include <linux/fs.h>
@@ -6,9 +7,8 @@
 #include <linux/moduleparam.h>
 #include <linux/printk.h>
 #include <linux/types.h>
-#include <asm/uaccess.h> 
 
-#include "ff.h"         
+#include "ff.h"
 
 #define DEVICE_NAME "Random_Numbers_Module"
 
@@ -17,8 +17,8 @@
 
 /* To forbidde multi-using */
 enum {
-        CDEV_NOT_USED = 0,
-        CDEV_EXCLUSIVE_OPEN = 1,
+  CDEV_NOT_USED = 0,
+  CDEV_EXCLUSIVE_OPEN = 1,
 };
 static atomic_t cdev_status = ATOMIC_INIT(CDEV_NOT_USED);
 
@@ -36,15 +36,17 @@ static ff_elem_t *ff_crs_elements[MAX_LENGTH];
 
 static int my_open(struct inode *inode, struct file *file);
 static int my_release(struct inode *inode, struct file *file);
-static ssize_t my_write(struct file *filp, const char __user *buffer, size_t length, loff_t *offset);
-static ssize_t my_read(struct file *filp, char __user *buffer, size_t length, loff_t *offset);
+static ssize_t my_write(struct file *filp, const char __user *buffer,
+                        size_t length, loff_t *offset);
+static ssize_t my_read(struct file *filp, char __user *buffer, size_t length,
+                       loff_t *offset);
 
 static struct file_operations fops = {
-        .owner = THIS_MODULE,
-        .open = my_open,
-        .release = my_release,
-        .write = my_write,
-        .read = my_read,
+    .owner = THIS_MODULE,
+    .open = my_open,
+    .release = my_release,
+    .write = my_write,
+    .read = my_read,
 };
 
 module_param(k_length, ulong, 0);
@@ -60,100 +62,93 @@ module_param(crs_c, byte, 0);
 MODULE_PARM_DESC(crs_c, "CRS constant");
 
 /* Init dev in Kernel */
-static int __init my_init(void)
-{
-        /* From the guide  */
-        major = register_chrdev(0, DEVICE_NAME, &fops);
-        if (major < 0) 
-        {
-                pr_alert("FAIL MAJOR %d\n", major);
-                return major;
-        }
-        pr_info("OK MAJOR%d\n", major);
+static int __init my_init(void) {
+  /* From the guide  */
+  major = register_chrdev(0, DEVICE_NAME, &fops);
+  if (major < 0) {
+    pr_alert("FAIL MAJOR %d\n", major);
+    return major;
+  }
+  pr_info("OK MAJOR%d\n", major);
 
-        /* From the guide */
-        cls = class_create(DEVICE_NAME);
-        device_create(cls, NULL, MKDEV(major, 0), NULL, DEVICE_NAME);
-        pr_info("OK DEVICE PATH /dev/%s\n", DEVICE_NAME);
+  /* From the guide */
+  cls = class_create(DEVICE_NAME);
+  device_create(cls, NULL, MKDEV(major, 0), NULL, DEVICE_NAME);
+  pr_info("OK DEVICE PATH /dev/%s\n", DEVICE_NAME);
 
-        /* Convert recieving number into f_{2^8}*/
-        for (size_t i = 0; i < k_length; i++) 
-        {
-                ff_crs_coefficients[i] = uint8_to_ff_elem(crs_coefficients[i]);
-                ff_crs_elements[i] = uint8_to_ff_elem(crs_elements[i]);
-        }
-        ff_crs_c = uint8_to_ff_elem(crs_c);
-        return SUCCESS;
+  /* Convert recieving number into f_{2^8}*/
+  for (size_t i = 0; i < k_length; i++) {
+    ff_crs_coefficients[i] = uint8_to_ff_elem(crs_coefficients[i]);
+    ff_crs_elements[i] = uint8_to_ff_elem(crs_elements[i]);
+  }
+  ff_crs_c = uint8_to_ff_elem(crs_c);
+  return SUCCESS;
 }
 
-static void __exit my_clean(void)
-{       
-        ff_elem_free(ff_crs_c);
-        for (size_t i = 0; i < k_length; ++i) {
-                ff_elem_free(ff_crs_coefficients[i]);
-                ff_elem_free(ff_crs_elements[i]);
-        }
-        device_destroy(cls, MKDEV(major, 0));
-        class_destroy(cls);
-        unregister_chrdev(major, DEVICE_NAME);
-        pr_info("OK clear\n");
-        return;
+static void __exit my_clean(void) {
+  ff_elem_free(ff_crs_c);
+  for (size_t i = 0; i < k_length; ++i) {
+    ff_elem_free(ff_crs_coefficients[i]);
+    ff_elem_free(ff_crs_elements[i]);
+  }
+  device_destroy(cls, MKDEV(major, 0));
+  class_destroy(cls);
+  unregister_chrdev(major, DEVICE_NAME);
+  pr_info("OK clear\n");
+  return;
 }
 
-static ssize_t my_write(struct file *file, const char __user *buffer, size_t count, loff_t *offset)
-{
-        /* We don't write, we run it through the Kernel parameters */
-        pr_alert("ERROR CAN'T WRITE.\n");
-        return -EINVAL; 
+static ssize_t my_write(struct file *file, const char __user *buffer,
+                        size_t count, loff_t *offset) {
+  /* We don't write, we run it through the Kernel parameters */
+  pr_alert("ERROR CAN'T WRITE.\n");
+  return -EINVAL;
 }
 
-static int my_open(struct inode *inode, struct file *file)
-{
-        /*compare-and-exchange*/
-        if (atomic_cmpxchg(&cdev_status, CDEV_NOT_USED, CDEV_EXCLUSIVE_OPEN)) {
-                return -EBUSY;
-        }
-        pr_info("OK OPEN\n");
-        try_module_get(THIS_MODULE);
-        return SUCCESS;
+static int my_open(struct inode *inode, struct file *file) {
+  /*compare-and-exchange*/
+  if (atomic_cmpxchg(&cdev_status, CDEV_NOT_USED, CDEV_EXCLUSIVE_OPEN)) {
+    return -EBUSY;
+  }
+  pr_info("OK OPEN\n");
+  try_module_get(THIS_MODULE);
+  return SUCCESS;
 }
 
-static int my_release(struct inode *inode, struct file *file)
-{
-        atomic_set(&cdev_status, CDEV_NOT_USED);
-        module_put(THIS_MODULE);
-        pr_info("OK CLOSE\n");
-        return SUCCESS;
+static int my_release(struct inode *inode, struct file *file) {
+  atomic_set(&cdev_status, CDEV_NOT_USED);
+  module_put(THIS_MODULE);
+  pr_info("OK CLOSE\n");
+  return SUCCESS;
 }
 
-static ssize_t my_read(struct file *file, char __user *buffer, size_t count, loff_t *offset)
-{
-        ff_elem_t *next_element, *product, *accumulated_sum, *tmp;
-        size_t i;
-        uint8_t byte_to_user = 0; 
-        next_element = NULL;
-        tmp = ff_get_zero(&ff_d8_p2);
-        for (i = 0; i < k_length; i++) 
-        {
-                product = ff_multiply(ff_crs_coefficients[i], ff_crs_elements[i]);
-                accumulated_sum = ff_sum(tmp, product);
-                ff_elem_free(product);
-                ff_elem_free(tmp);
-                tmp = accumulated_sum;
-        }        
-        next_element = ff_sum(tmp, ff_crs_c);
-        ff_elem_free(tmp);
-        ff_elem_free(ff_crs_elements[0]);
-        memmove(ff_crs_elements, ff_crs_elements + 1, (k_length - 1) * sizeof(ff_elem_t*                 ));
+static ssize_t my_read(struct file *file, char __user *buffer, size_t count,
+                       loff_t *offset) {
+  ff_elem_t *next_element, *product, *accumulated_sum, *tmp;
+  size_t i;
+  uint8_t byte_to_user = 0;
+  next_element = NULL;
+  tmp = ff_get_zero(&ff_d8_p2);
+  for (i = 0; i < k_length; i++) {
+    product = ff_multiply(ff_crs_coefficients[i], ff_crs_elements[i]);
+    accumulated_sum = ff_sum(tmp, product);
+    ff_elem_free(product);
+    ff_elem_free(tmp);
+    tmp = accumulated_sum;
+  }
+  next_element = ff_sum(tmp, ff_crs_c);
+  ff_elem_free(tmp);
+  ff_elem_free(ff_crs_elements[0]);
+  memmove(ff_crs_elements, ff_crs_elements + 1,
+          (k_length - 1) * sizeof(ff_elem_t *));
 
-        ff_crs_elements[k_length - 1] = next_element;
-        byte_to_user = ff_elem_to_uint8(next_element);
-        
-        if (copy_to_user(buffer, &byte_to_user, sizeof(byte_to_user))) 
-        {
-                pr_err("ERROR COPY TO USER.\n");
-        }
-        return 1;
+  ff_crs_elements[k_length - 1] = next_element;
+  byte_to_user = ff_elem_to_uint8(next_element);
+
+  if (copy_to_user(buffer, &byte_to_user, sizeof(byte_to_user))) {
+    pr_err("ERROR COPY TO USER.\n");
+  }
+  return 1;
 }
 
 module_init(my_init);
